8b6e07fddd257f876ed99e2796f18aee385b532a
kim hyeon su <khs1024zxzx@naver.com>
2026-04-02 11:05:34 +0900

    chore: remove nested project folders

--- teamProject1/src/main/java/com/example/teamProject1/repository/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.teamProject1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.teamProject1.entity.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Integer> {
-}
