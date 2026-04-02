eb599ba0a0a6ba4418730910a8b9e324b47ee857
KwanHoYoo <ykh4343@naver.com>
2026-04-02 20:26:00 +0900

    fix: 패키지명 cowork로 변경 및 오류 수정

---
+++ src/main/java/com/example/cowork/Application.java
@@ -0,0 +1,13 @@
+package com.example.cowork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
