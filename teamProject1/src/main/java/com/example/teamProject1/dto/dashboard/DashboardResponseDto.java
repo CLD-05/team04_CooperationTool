3d855efdb8c867d3f513147b25e22c9ebb93a880
namoo0515 <eunjikim0211@gmail.com>
2026-04-02 10:12:56 +0900

    feat: 작업파일추가

---
+++ teamProject1/src/main/java/com/example/teamProject1/dto/dashboard/DashboardResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.teamProject1.dto.dashboard;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DashboardResponseDto {
+	
+	private String userName;
+	private List<TeamCardDto> teams;
+	
+	private int totalPages;
+	private long totalElements;
+}
